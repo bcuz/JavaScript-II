33686a482bd56930349c9299759e90d5a23ecb7b
adam <bcuz@users.noreply.github.com>
2018-12-20 08:33:54 -0500

    closure stretch

--- closure.js
+++ closure.js
@@ -32,4 +32,22 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  let counter = 0;
+  return {
+    increment() {
+      counter += 1; 
+      return counter;
+    },
+    decrement() {
+      counter -= 1; 
+      return counter;
+    }
+  }
+
 };
+
+const advCounter = counterFactory();
+console.log(advCounter.increment()); // 1
+console.log(advCounter.increment()); // 2
+console.log(advCounter.decrement()); // 1
