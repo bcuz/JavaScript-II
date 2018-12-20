21e256f8776f462100b637f6079fb7a09660094c
adam <bcuz@users.noreply.github.com>
2018-12-20 08:17:27 -0500

    closures

--- closure.js
+++ closure.js
@@ -1,14 +1,29 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+// IIFE
+var myFunc = (function () {
+  var name = 'bob';
+  return function() {
+    console.log(name);    
+  }
+})();
+
+myFunc();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 0;
+  return function () {
+    counter += 1; 
+    return counter;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage: 
+// const newCounter = counter();
+// console.log(newCounter()); // 1
+// console.log(newCounter()); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
